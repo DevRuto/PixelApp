5481342150e49bf9b9fb3f956ce296b9bbbe61b7
Ruto <ruto@protonmail.ch>
2020-08-30 22:03:07 -0400

    Responsive menu

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,10 +6,13 @@
       </div>
       <div class="block lg:hidden">
         <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white" @click="toggleMenu">
-          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+          <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
         </button>
       </div>
-      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto" v-bind:class="{hidden: menuActive}">
         <div class="text-sm lg:flex-grow">
           <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
             Home
@@ -24,9 +27,12 @@
 <script lang="ts">
 import Vue from 'vue';
 export default Vue.extend({
+  data: () => ({
+    menuActive: false
+  }),
   methods: {
     toggleMenu () {
-
+      this.menuActive = !this.menuActive;
     }
   }
 });
